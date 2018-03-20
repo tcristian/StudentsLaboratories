1a38d923378d9d435ad0d38a2e14c43a6fda6301
AlexA3 <alexpop967@gmail.com>
2018-03-20 20:01:47 +0200

    pom update

---
+++ src/test/java/controller/LaboratoriesControllerTest.java
@@ -0,0 +1,34 @@
+package controller;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Alex on 20.03.2018.
+ */
+public class LaboratoriesControllerTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testSaveStudent() throws Exception {
+        assertTrue(true);
+    }
+
+    public void testSaveLaboratory() throws Exception {
+
+    }
+
+    public void testAddGrade() throws Exception {
+
+    }
+
+    public void testPassedStudents() throws Exception {
+
+    }
+
+}
